8456a529959406936748c8ceba3d899fb8d00556
Mariemhachichi <hchichimariem@hotmail.fr>
2025-04-17 14:47:52 +0100

    views auth

--- server.js
+++ server.js
@@ -1,51 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config(); // Pour utiliser .env
+require('dotenv').config();
 
 // Import des routes
 const authRoute = require('./routes/AuthRoute');
 const rdvRoute = require('./routes/RdvRoute');
 
-// Création de l'application Express
 const app = express();
 const Port = process.env.PORT || 9000;
 
-// Configuration du moteur de vues EJS
+// Configuration EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); 
 
-// Middleware pour lire les données envoyées dans les formulaires (POST)
+// Middleware pour les données de formulaires
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Fichiers statiques (Bootstrap, CSS, images, etc.)
-//app.use(express.static(path.join(__dirname, 'public')));
+// Middleware pour Css
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-// Routes d'affichage des pages HTML (avant les routes API)
+// Routes pour les pages
 app.get('/login', (req, res) => {
-    console.log('Route /login atteinte'); // Affiche ce message dans la console
-    res.render('./views/login'); // Affiche login.ejs
-  });
-
-app.get('/register', (req, res) => {
-  res.render('register'); // Affiche register.ejs
+    res.render('login'); 
 });
 
-// Routes API (inscription, connexion, rdv...)
+app.get('/register', (req, res) => {
+    res.render('register'); 
+});
+
+// Routes API
 app.use('/', authRoute);
 app.use('/', rdvRoute);
 
-//  Connexion à MongoDB
+// Connexion MongoDB
 mongoose.connect(process.env.URL_BD)
-  .then(() => {
-    console.log("Connecté à la base de données !");
-  })
-  .catch((err) => {
-    console.error("Erreur de connexion à la base de données :", err);
-  });
+    .then(() => console.log("Connecté à MongoDB !"))
+    .catch(err => console.error("Erreur MongoDB :", err));
 
-// Lancement du serveur
+// Démarrage du serveur
 app.listen(Port, () => {
-  console.log(` Serveur lancé sur http://localhost:${Port}`);
+    console.log(`Serveur démarré sur:${Port}`);
 });
